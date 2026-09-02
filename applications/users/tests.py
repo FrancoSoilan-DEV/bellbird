b5cfcbba962b17bd89cd59a85bc94bf49a7ef822
FrancoSoilan-DEV <soilanbackend@gmail.com>
2026-09-02 14:48:37 -0300

    test: agregar tests de login y proteccion de dashboard (RED) -(el commit anterior fue soobreescrito)

--- applications/users/tests.py
+++ applications/users/tests.py
@@ -1,83 +1,26 @@
-from django.contrib.auth.models import AnonymousUser
-from django.http import HttpResponse
-from django.test import RequestFactory, TestCase
-from django.views import View
-from django.core.exceptions import PermissionDenied
+from django.test import TestCase
+from django.urls import reverse
 
-from applications.users.mixins import RoleRequiredMixin
 from applications.users.models import User
 
 
-class _EmployeeOnlyView(RoleRequiredMixin, View):
-    required_role = 'is_employee'
-
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('ok')
-
-
-class _ResponsibleOnlyView(RoleRequiredMixin, View):
-    required_role = 'is_responsible'
-
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('ok')
-
-
-class RoleRequiredMixinTests(TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-
-    def test_user_with_matching_role_can_access(self):
-        user = User.objects.create_user(
-            username='empleado1', password='pass',
+class CustomLoginViewTests(TestCase):
+    def test_employee_login_redirects_to_employee_dashboard(self):
+        User.objects.create_user(
+            username='empleado1', password='pass123',
             is_employee=True, is_responsible=False,
         )
-        request = self.factory.get('/dummy/')
-        request.user = user
 
-        response = _EmployeeOnlyView.as_view()(request)
+        response = self.client.post(reverse('login'), {
+            'username': 'empleado1',
+            'password': 'pass123',
+        })
 
-        self.assertEqual(response.status_code, 200)
+        self.assertRedirects(response, reverse('e-dash'))
 
-    def test_user_without_matching_role_gets_forbidden(self):
-        user = User.objects.create_user(
-            username='responsable1', password='pass',
-            is_employee=False, is_responsible=True,
-        )
-        request = self.factory.get('/dummy/')
-        request.user = user
 
-        with self.assertRaises(PermissionDenied):
-            _EmployeeOnlyView.as_view()(request)
-
+class DashboardProtectionTests(TestCase):
     def test_anonymous_user_is_redirected_to_login(self):
-        request = self.factory.get('/dummy/')
-        request.user = AnonymousUser()
-
-        response = _EmployeeOnlyView.as_view()(request)
+        response = self.client.get(reverse('e-dash'))
 
         self.assertEqual(response.status_code, 302)
-
-    def test_responsible_only_view_rejects_pure_employee(self):
-        user = User.objects.create_user(
-            username='empleado2', password='pass',
-            is_employee=True, is_responsible=False,
-        )
-        request = self.factory.get('/dummy/')
-        request.user = user
-
-        with self.assertRaises(PermissionDenied):
-            _ResponsibleOnlyView.as_view()(request)
-
-    def test_user_with_both_roles_can_access_both_views(self):
-        user = User.objects.create_user(
-            username='ambos', password='pass',
-            is_employee=True, is_responsible=True,
-        )
-        request = self.factory.get('/dummy/')
-        request.user = user
-
-        employee_response = _EmployeeOnlyView.as_view()(request)
-        responsible_response = _ResponsibleOnlyView.as_view()(request)
-
-        self.assertEqual(employee_response.status_code, 200)
-        self.assertEqual(responsible_response.status_code, 200)
